feee17632cb8ef9a483ca04806676b574529e8de
Himanshu <himukumar005@gmail.com>
2022-05-25 19:27:01 +0530

    Create Kadane's Algorithm.cpp

---
+++ Arrays/Kadane's Algorithm.cpp
@@ -0,0 +1,25 @@
+//Given an array Arr[] of N integers. Find the contiguous sub-array(containing at least one number) which has the maximum sum and return its sum.
+
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+    int max_so_far = INT_MIN, max_ending_here = 0;
+ 
+    for (int i = 0; i <n; i++)
+    {
+        max_ending_here = max_ending_here + arr[i];
+        if (max_so_far < max_ending_here)
+            max_so_far = max_ending_here;
+ 
+        if (max_ending_here < 0)
+            max_ending_here = 0;
+    }
+    return max_so_far;
+        
+    }
+};
